59261c15ce8697b6d270952243ef6e4d4467c3f7
Catherine Claudet <cmclaudet@gmail.com>
2026-01-31 13:01:29 +0100

    Add ability to trigger dialogue distractions

--- src/dialogue.gd
+++ src/dialogue.gd
@@ -6,30 +6,63 @@ extends Node2D
 @onready var rich_text_label: RichTextLabel = %RichTextLabel
 
 var tween: Tween
-var lines: PackedStringArray
+var main_lines: PackedStringArray
 var current_line_index: int = 0
 
-func _ready() -> void:
-	lines = get_dialogue_lines()
-	current_line_index = 0
-	scroll_next_line()
+var distracted_lines: Array[PackedStringArray]
+var distracted_level: int = 0
+var is_distracted_text: bool
 
-func get_dialogue_lines() -> PackedStringArray:
+func _ready() -> void:
+	main_lines = load_dialogue_lines()
+	distracted_lines = load_distracted_lines()
+	current_line_index = 0
+	scroll_text(main_lines[current_line_index])
+
+func _process(_delta: float) -> void:
+	# temporary debug to press D
+	if Input.is_key_pressed(KEY_D):
+		on_distracted()
+
+func on_distracted() -> void:
+	if !is_distracted_text:
+		var line = get_distracted_line()
+		is_distracted_text = true
+		scroll_text(line)
+
+func get_distracted_line() -> String:
+	var lines: PackedStringArray
+	if (distracted_level < distracted_lines.size()):
+		lines = distracted_lines[distracted_level]
+	else:
+		lines = distracted_lines[distracted_lines.size() - 1]
+
+	var line_index = randf_range(0, lines.size())
+	return lines[line_index]
+
+func load_dialogue_lines() -> PackedStringArray:
 	var file = FileAccess.open("res://dialogues/test.txt", FileAccess.READ)
 	var text = file.get_as_text()
-	lines = text.split("\n")
+	var split_lines = text.split("\n")
 	var non_empty_lines: PackedStringArray = []
-	for line in lines:
+	for line in split_lines:
 		if line.strip_edges() != "":
 			non_empty_lines.append(line)
 	return non_empty_lines
 
-func scroll_next_line() -> void:
-	if current_line_index >= lines.size():
-		return
-
-	scroll_text(lines[current_line_index])
-	current_line_index += 1
+func load_distracted_lines() -> Array[PackedStringArray]:
+	var file = FileAccess.open("res://dialogues/annoyed.txt", FileAccess.READ)
+	var text = file.get_as_text()
+	var split_lines = text.split("\n")
+	var line_block: PackedStringArray = []
+	var lines: Array[PackedStringArray] = []
+	for line in split_lines:
+		if line.strip_edges() != "":
+			line_block.append(line)
+		else:
+			lines.append(line_block)
+			line_block = []
+	return lines
 
 func scroll_text(text: String) -> void:
 	if tween:
@@ -44,3 +77,14 @@ func scroll_text(text: String) -> void:
 	tween = create_tween()
 	tween.tween_property(rich_text_label, "visible_characters", total_characters, duration)
 	tween.tween_callback(scroll_next_line).set_delay(delay_between_lines)
+
+func scroll_next_line() -> void:
+	if is_distracted_text:
+		is_distracted_text = false
+	else:
+		current_line_index += 1
+
+	if current_line_index >= main_lines.size():
+		return
+
+	scroll_text(main_lines[current_line_index])

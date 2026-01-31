d2a8720a861918f506a1d65cc65be6f270516a1e
boesh <dephiloper@users.noreply.github.com>
2026-01-31 14:52:30 +0100

    make api available for distraction value

--- src/distraction_manager.gd
+++ src/distraction_manager.gd
@@ -2,7 +2,7 @@ class_name DistractionManager extends Node
 
 var distraction_pool: Array = []
 
-var _active_distraction: Distraction = null
+var active_distraction: Distraction = null
 var _last_distraction_id: String = ""
 
 @onready var distraction_timer: Timer = %Timer
@@ -24,21 +24,21 @@ func _ready() -> void:
 
 
 func _process(_delta: float) -> void:
-	if not _active_distraction:
+	if not active_distraction:
 		var no_duplicate_distraction_pool := distraction_pool.filter(
 			func(dist): return dist.distraction_id != _last_distraction_id
 		)
 
 		var distraction: Distraction = no_duplicate_distraction_pool.pick_random().duplicate()
 		add_child(distraction)
-		_active_distraction = distraction
+		active_distraction = distraction
 		distraction_timer.start()
 		print("new distraction started '%s'" % distraction.distraction_id)
 
 
 func _on_distraction_timer_timeout() -> void:
-	if _active_distraction:
-		_last_distraction_id = _active_distraction.distraction_id
+	if active_distraction:
+		_last_distraction_id = active_distraction.distraction_id
 		print("last distraction id:", _last_distraction_id)
-		_active_distraction.queue_free()
-		_active_distraction = null
+		active_distraction.queue_free()
+		active_distraction = null

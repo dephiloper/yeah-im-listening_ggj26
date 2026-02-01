b55667062a2988e82aa2880e99b4dce5e7089552
Catherine Claudet <cmclaudet@gmail.com>
2026-02-01 12:30:32 +0100

    Add dialogue option timer; make it only two options instead of three; move dialogue options down

--- src/dialogue.gd
+++ src/dialogue.gd
@@ -4,6 +4,7 @@ var option_prefix: String = "[?]"
 
 @export var characters_per_second: float = 30.0
 @export var delay_between_lines: float = 3.0
+@export var time_option: float = 10.0
 
 @export var max_shake_rate: int = 50
 @export var max_shake_offset: int = 10
@@ -12,6 +13,8 @@ var option_prefix: String = "[?]"
 @onready var distraction_manager: DistractionManager = %DistractionManager
 
 @export var dialogue_options: Array[Button]
+@export var option_timer_container: ColorRect
+@export var option_timer_bar: ColorRect
 
 var tween: Tween
 var current_line_index: int = 0
@@ -27,6 +30,8 @@ var current_branch_lines: PackedStringArray = []
 var current_branch_index: int = -1
 var is_in_branch: bool = false
 var waiting_for_option: bool = false
+var time_since_option_start: float = 0
+var timer_bar_original_width: float
 
 class DialogueEntry:
 	var text: String = ""
@@ -45,9 +50,18 @@ func _ready() -> void:
 	current_line_index = 0
 	hide_dialogue_options()
 	scroll_text(dialogue_entries[current_line_index].text)
+	timer_bar_original_width = option_timer_bar.size.x
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	rich_text_label.text = get_text()
+	if waiting_for_option:
+		time_since_option_start += delta
+		var ratio = clamp(time_since_option_start / time_option, 0.0, 1.0)
+		option_timer_bar.size.x = lerp(timer_bar_original_width, 0.0, ratio)
+		if time_since_option_start >= time_option:
+			var current_entry = dialogue_entries[current_line_index]
+			select_option(current_entry.options.size() - 1)
+
 
 func get_text() -> String:
 	var distraction_intesity: float = 0
@@ -170,12 +184,16 @@ func show_dialogue_options(options: Array[DialogueOption]) -> void:
 			dialogue_options[i].visible = true
 		else:
 			dialogue_options[i].visible = false
+	option_timer_container.visible = true
+	option_timer_bar.size.x = timer_bar_original_width
 
 func hide_dialogue_options() -> void:
 	for option_label in dialogue_options:
 		option_label.visible = false
+	option_timer_container.visible = false
 
 func select_option(option_index: int) -> void:
+	time_since_option_start = 0
 	if not waiting_for_option:
 		return
 
